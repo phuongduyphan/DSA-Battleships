67b4245a03a10f62b3cb026b08319da73422f32f
Chau Le <nganchau.lenguyen>
2018-04-27 19:38:44 +0700

    Update NormalMode

--- src/log/Log.java
+++ src/log/Log.java
@@ -3,7 +3,6 @@ package log;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class Log {
 	public static Logger logger;
